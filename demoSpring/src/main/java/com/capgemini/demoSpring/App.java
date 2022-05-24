992207972dabdac8adc63ecef175a43ed3b4f7e1
diego <diego.cortinagarcia@gmail.com>
2022-05-24 13:41:27 +0200

    bean called

--- demoSpring/src/main/java/com/capgemini/demoSpring/App.java
+++ demoSpring/src/main/java/com/capgemini/demoSpring/App.java
@@ -1,13 +1,24 @@
 package com.capgemini.demoSpring;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.capgemini.beans.*;
+
+public class App {
+	public static void main(String[] args) {
+		
+		HolaMundo m = new HolaMundo();
+		m.setSaludo("Eseeee");
+		System.out.println(m.getSaludo());
+
+		ApplicationContext appContext = new ClassPathXmlApplicationContext("com/capgemini/xmls/beans.xml");
+
+		HolaMundo hola = (HolaMundo) appContext.getBean("mundo"); // se usa el id que se puso en el beans.xml
+
+		System.out.println(hola.getSaludo());
+
+		((ConfigurableApplicationContext) appContext).close();
+	}
+
 }
