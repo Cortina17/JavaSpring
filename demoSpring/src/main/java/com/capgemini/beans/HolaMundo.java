a2f052e0a9e6dba4ded0fd3589774120e6157bd7
diego <diego.cortinagarcia@gmail.com>
2022-05-24 12:53:46 +0200

    Esto es un Bean. POJO plain old java object

---
+++ demoSpring/src/main/java/com/capgemini/beans/HolaMundo.java
@@ -0,0 +1,15 @@
+package com.capgemini.beans;
+
+public class HolaMundo {
+	
+	String saludo;
+
+	public String getSaludo() {
+		return saludo;
+	}
+
+	public void setSaludo(String saludo) {
+		this.saludo = saludo;
+	}
+
+}
