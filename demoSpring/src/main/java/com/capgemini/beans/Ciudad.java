eddab336ca486096c8edb9d5b5515a3372beb98d
diego <diego.cortinagarcia@gmail.com>
2022-05-26 10:42:52 +0200

    added jakarta

--- demoSpring/src/main/java/com/capgemini/beans/Ciudad.java
+++ demoSpring/src/main/java/com/capgemini/beans/Ciudad.java
@@ -1,13 +1,18 @@
 package com.capgemini.beans;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 public class Ciudad {
 	
 	private String nombre;
 	
+	@PostConstruct
 	private void init() {
 		System.out.println("antes de iniciar el bean ciu");
 	}
 	
+	@PreDestroy
 	private void destroy() {
 		System.out.println("apunto de destruir el bean ciu");
 	}
